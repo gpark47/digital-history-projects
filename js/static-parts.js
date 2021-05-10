11ba1843b9c54d88e0231ca33457458ef4a52058
gpark47 <gracepark47@gmail.com>
2021-05-09 22:24:24 -0400

    think

--- js/static-parts.js
+++ js/static-parts.js
@@ -18,7 +18,7 @@ let authorName='Grace Park',
 
 
 function makeMenu (items= menuEntries) {
-  let prefix = '',
+  let prefix = '/',
       html = '',
       basedir = 'https://gpark47.github.io/digital-history-projects'
      // basedir = window.location.pathname.split(/\//g).splice(-2)[0]; // check which dir we're in
